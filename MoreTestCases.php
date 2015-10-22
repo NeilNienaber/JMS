aa3512c691e9ac49ecacef071ff3dbf415b77d6d
NeilNienaber <neil.nienaber@gmail.com>
2015-10-22 14:03:47 +0200

    more testing executor values
    more testing executor values

--- MoreTestCases.php
+++ MoreTestCases.php
@@ -8,6 +8,7 @@ class MoreTestCases extends \PHPUnit_Framework_TestCase
  */
   public function testThisIsRandom()
   {
+	print_r("THE EXECUTOR" . getenv("EXECUTOR_NUMBER"));
 	$this->assertTrue(true);
 	//$this->fail('THIS IS A FAILURE');
   }	
